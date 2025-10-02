3811c710500e6fd5595b63e51c965608a3a3f4f7
vaishnavi-bhartiya <vaishnavimzp@gmail.com>
2025-10-02 17:31:05 +0530

    initial Commit

---
+++ greeting_card.py
@@ -0,0 +1,3 @@
+a = input("Enter your first name:")
+b = input("Enter your last name:")
+print(f"\nHello, {a + ' ' + b}! Welcome to the Python program.")
---
+++ maths_operations.py
@@ -0,0 +1,7 @@
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(f"\nAddition: ",a+b)
+print(f"Subtraction: ",a-b)
+print(f"Multiplication: ",a*b)
+print(f"Division: ",a/b)
